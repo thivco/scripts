8b38bf23d11c82d662511c3a1d50957905bbd592
thivco <thibault.vivier@epitech.eu>
2024-11-06 11:53:16 +0100

    Cleaned up code

--- crud.php
+++ crud.php
@@ -1,33 +1,46 @@
+#!/usr/bin/env php
 <?php
+// Script that behaves similarly to doctrine
+// Create MVC files, adds entries and fetches the databsae
 
 function main($argv, $argc){
-	$entity_fields=[];
 	echo $argc . " arguments" . PHP_EOL;
 	if($argv[2] == "entity"){
-		echo "Generating entity..." . PHP_EOL;
-		$entity_name = readline("Entity name :" . PHP_EOL);
-		echo $entity_name;	
-		$entity_done = false;
-		$entity_field_name = "";
-		while(!$entity_done){
-			while($entity_field_name == ""){	
-				$entity_field_name = readline("Name of the field (type \"done\" to exit)");
-			}
-			if($entity_field_name == "done"){
-				$entity_done = true;
-			}
-			$entity_field_type = "";
-			$entity_field_type = readline("Define the type (string, str, int or integer)");
-			array_push($entity_fields, $entity_field_name, $entity_field_type);	
-			$entity_field_name = "";
-			$entity_field_type = "";
-		}
-			print_r($entity_fields);
+		generate_entity();
 	}
-	else if($argv[2] == "controller"){
-		echo "Generating controller" . PHP_EOL;
+	
+}
+
+function generate_entity(){
+	echo "Generating entity..." . PHP_EOL;
+
+	$entity_fields=[];
+	$entity_name = "";
+	$entity_field_name = "";
+	$entity_done = false;
+
+	while($entity_name == ""){	
+		$entity_name = readline("Entity name :" . PHP_EOL);
 	}
 
+	while(!$entity_done){
+	
+		while($entity_field_name == ""){	
+			$entity_field_name = readline("Name of the field (type \"done\" to exit)");
+		}
+	
+		if($entity_field_name == "done"){
+			$entity_done = true;
+		}
+	
+		$entity_field_type = readline("Define the type (string, str, int or integer)");
+		array_push($entity_fields, $entity_field_name, $entity_field_type);	
+
+		$entity_field_type = "";
+		$entity_field_name = "";
+		$entity_field_type = "";
+	}
+		print_r($entity_fields);
 }
 
 main($argv, $argc);
